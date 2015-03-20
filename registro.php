df224c2a6afdef6025533d7d01cd61a8cf34df5b
Uriel Escorcia <uris_ec@hotmail.com>
2015-03-20 17:51:07 -0600

    arreglo registro usr

--- registro.php
+++ registro.php
@@ -6,6 +6,13 @@
 
 	<title>Finder</title>
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<!-- Add mousewheel plugin (this is optional) -->
+	<script type="text/javascript" src="js/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+
+	<!-- Add fancyBox -->
+	<link rel="stylesheet" href="js/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+	<script type="text/javascript" src="js/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+
 	<script src="js/init.js"></script>
 </head>
  
